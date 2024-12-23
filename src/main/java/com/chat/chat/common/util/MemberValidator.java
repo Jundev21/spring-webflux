ca1f5e853dd3a255286ecb3e123cf5f3125c3aec
acroSDH <dhseo@acrofuture.com>
2024-12-23 16:29:24 +0900

    fix: memberValidator 안의 exception-> customException으로 변경

--- src/main/java/com/chat/chat/common/util/MemberValidator.java
+++ src/main/java/com/chat/chat/common/util/MemberValidator.java
@@ -1,5 +1,6 @@
 package com.chat.chat.common.util;
 
+import com.chat.chat.common.exception.CustomException;
 import com.chat.chat.dto.request.MemberRequest;
 
 public class MemberValidator {
@@ -8,10 +9,10 @@ public class MemberValidator {
 
     public static void validate(MemberRequest memberRequest) {
         if (!memberRequest.getMemberId().matches("^[a-zA-Z0-9]{5,15}$")) {
-            throw new IllegalArgumentException("Invalid member id");
+            throw new CustomException("Invalid member id");
         }
         if (!memberRequest.getMemberPassword().matches("^(?=.*[A-Z])(?=.*\\d).{8,}$")) {
-            throw new IllegalArgumentException("Invalid member password");
+            throw new CustomException("Invalid member password");
         }
 
     }
