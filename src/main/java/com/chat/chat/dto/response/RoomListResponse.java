786ada74fb5b068ac039febfdf4264d6192ef9a6
jun <jundev21@gmail.com>
2024-12-27 13:48:19 +0900

    [REFACTOR] 방 전체조회 응답값, 방 생성 리팩토링

---
+++ src/main/java/com/chat/chat/dto/response/RoomListResponse.java
@@ -0,0 +1,23 @@
+package com.chat.chat.dto.response;
+
+import java.util.List;
+
+import com.chat.chat.entity.Room;
+
+public record RoomListResponse(
+
+	String roomName,
+	String adminMemberId,
+	List<BasicMemberResponse> groupMember
+) {
+
+		public static RoomListResponse roomListResponse(Room room, List<BasicMemberResponse> basicMemberResponses){
+			return new RoomListResponse(
+				room.getRoomName(),
+				room.getAdminMemberId(),
+				basicMemberResponses
+			);
+		}
+
+
+	}
