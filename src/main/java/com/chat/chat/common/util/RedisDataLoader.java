10e97c40f1da9603a392a7d370888d910fb5ab95
acroSDH <dhseo@acrofuture.com>
2024-12-27 11:29:08 +0900

    [FEAT]:redis로직을 repository 로 추상화

--- src/main/java/com/chat/chat/common/util/RedisDataLoader.java
+++ src/main/java/com/chat/chat/common/util/RedisDataLoader.java
@@ -2,11 +2,11 @@ package com.chat.chat.common.util;
 
 import com.chat.chat.entity.Member;
 import com.chat.chat.entity.Room;
+import com.chat.chat.repository.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -16,7 +16,7 @@ import reactor.core.publisher.Flux;
  * -> 대책으로 서버로드시 "미리로드"하는 preloading 을 사용
  * <p>
  * 2. webflux 비동기 = redis 에서도 비동기 사용
- * -> ReactiveRedisTemplate 사용
+ * -> ReactiveRedisTemplate
  * -> Mongo 에서 가져오는 데이터 여러개 = flux
  * <p>
  * 3.저장할 데이터
@@ -32,23 +32,25 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class RedisDataLoader {
 
-    private final ReactiveRedisTemplate<String, String> redisTemplate;
+    private final RedisRepository redisRepository;
     private final ReactiveMongoTemplate mongoTemplate;
 
     @Bean
     public ApplicationRunner loadDataToRedisBeforeServerStart() {
         return args -> {
+
             Flux<Member> members = mongoTemplate.findAll(Member.class);
 
             members.subscribe(member -> {
-                redisTemplate.opsForValue().set("memberId:" + member.getId(), member.getMemberId()).subscribe();
-                redisTemplate.opsForValue().set("memberPw:" + member.getId(), member.getMemberPassword()).subscribe();
+                redisRepository.save("memberId:" + member.getMemberId(), member.getMemberId()).subscribe();
+                redisRepository.save("memberPw:" + member.getMemberId(), member.getMemberPassword()).subscribe();
             });
+
 
             Flux<Room> rooms = mongoTemplate.findAll(Room.class);
 
             rooms.subscribe(room -> {
-                redisTemplate.opsForValue().set("roomName:" + room.getId(), room.getRoomName()).subscribe();
+                redisRepository.save("roomName:" + room.getRoomName(), room.getRoomName()).subscribe();
             });
 
             System.out.println("Redis Load Complete!");
