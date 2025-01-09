43816f205b4b349299bea98f5fde392e03088a79
jun <jundev21@gmail.com>
2025-01-09 16:49:09 +0900

    [FIX] 주석 제거

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 commit type
 [FEAT] : 새로운 기능 추가
 [FIX] : 버그 수정
+
 [DOCS] : 문서 수정
+
 [REFACTOR] : 코드 리펙토링
+
 [TEST] : 테스트 코드, 리펙토링 테스트 코드 추가
+
+
+	//reactive mongodb pagenation 정보
+	// https://www.devkuma.com/docs/spring-data-mongo/pagination/
+	// https://medium.com/@AADota/spring-mongo-template-pagination-92fa93c50d5c
+	// https://gnidinger.tistory.com/entry/WebFluxReactive-MongoDB-%EC%BF%BC%EB%A6%AC-%EC%A0%95%EB%A0%AC-%ED%8E%98%EC%9D%B4%EC%A7%80%EB%84%A4%EC%9D%B4%EC%85%98
+	//pageable 성능최적화
+	// https://junior-datalist.tistory.com/342
--- src/main/java/com/chat/chat/service/RoomService.java
+++ src/main/java/com/chat/chat/service/RoomService.java
@@ -40,12 +40,6 @@ public class RoomService {
 	private final CustomRepository customRepository;
 	private final RepositorySelector repositorySelector;
 
-	//reactive mongodb pagenation 정보
-	// https://www.devkuma.com/docs/spring-data-mongo/pagination/
-	// https://medium.com/@AADota/spring-mongo-template-pagination-92fa93c50d5c
-	// https://gnidinger.tistory.com/entry/WebFluxReactive-MongoDB-%EC%BF%BC%EB%A6%AC-%EC%A0%95%EB%A0%AC-%ED%8E%98%EC%9D%B4%EC%A7%80%EB%84%A4%EC%9D%B4%EC%85%98
-	//pageable 성능최적화
-	// https://junior-datalist.tistory.com/342
 	public Mono<Page<RoomListResponse>> getAllRooms(String page, String size) {
 		Pageable pageable = PageRequest.of(Integer.parseInt(page), Integer.parseInt(size));
 		Mono<List<Room>> roomsMono = customRepository.findAllRoomWithPageNation(pageable);
